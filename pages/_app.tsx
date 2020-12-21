e205931dc2300de800767d4fbab87305e7efa7cb
peterennis <peterennis@yahoo.com>
2020-12-20 19:02:29 -0800

    feat(): ionic example without grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,60 @@
-import React, { useEffect } from 'react'
-import { defineCustomElements as ionDefineCustomElements } from '@ionic/core/loader'
+import React, { useEffect } from "react";
+import { defineCustomElements as ionDefineCustomElements } from "@ionic/core/loader";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/core/css/core.css'
+import "@ionic/core/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/core/css/normalize.css'
-import '@ionic/core/css/structure.css'
-import '@ionic/core/css/typography.css'
+import "@ionic/core/css/normalize.css";
+import "@ionic/core/css/structure.css";
+import "@ionic/core/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/core/css/padding.css'
-import '@ionic/core/css/float-elements.css'
-import '@ionic/core/css/text-alignment.css'
-import '@ionic/core/css/text-transformation.css'
-import '@ionic/core/css/flex-utils.css'
-import '@ionic/core/css/display.css'
+import "@ionic/core/css/padding.css";
+import "@ionic/core/css/float-elements.css";
+import "@ionic/core/css/text-alignment.css";
+import "@ionic/core/css/text-transformation.css";
+import "@ionic/core/css/flex-utils.css";
+import "@ionic/core/css/display.css";
+
+const divStyle = {
+  color: "darkgreen",
+};
+
+const logoStyle = {
+  height: "1em",
+  margin: "0px",
+};
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    ionDefineCustomElements(window)
-  })
+    ionDefineCustomElements(window);
+  });
   return (
     <ion-app>
       <ion-header translucent>
         <ion-toolbar>
-          <ion-title>Next.js with Ionic</ion-title>
+          <ion-title style={divStyle}>Next.js with Ionic</ion-title>
         </ion-toolbar>
       </ion-header>
-
       <ion-content fullscreen>
         <Component {...pageProps} />
       </ion-content>
       <ion-footer>
         <ion-toolbar>
-          <ion-title>Footer</ion-title>
+          <ion-title>
+            an adaept{" "}
+            <img
+              style={logoStyle}
+              src="/assets/icon/favicon.png"
+              alt="adaept logo"
+            />{" "}
+            project
+          </ion-title>
         </ion-toolbar>
       </ion-footer>
     </ion-app>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,29 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <ion-grid>
-      <ion-row>
-        {new Array(8).fill('').map((k, i) => (
-          <ion-col key={i} size="3">
-            <ion-card>
-              <Image
-                src="/cat.jpg"
-                alt="Picture of the author"
-                width={500}
-                height={500}
-              />
-              <ion-card-header>
-                <ion-card-subtitle>Destination</ion-card-subtitle>
-                <ion-card-title>Madison, WI</ion-card-title>
-              </ion-card-header>
-              <ion-card-content>
-                <ion-icon name="pin" slot="start"></ion-icon>
-                Keep close to Nature's heart... and break clear away, once in
-                awhile, and climb a mountain or spend a week in the woods. Wash
-                your spirit clean.
-              </ion-card-content>
-            </ion-card>
-          </ion-col>
-        ))}
-      </ion-row>
-    </ion-grid>
-  )
+    <ion-row>
+      {new Array(8).fill("").map((k, i) => (
+        <ion-col key={i} size="auto">
+          <ion-card>
+            <Image
+              src="/ae-octocat-red.png"
+              alt="Cool Katze"
+              width={400}
+              height={400}
+            />
+            <ion-card-header>
+              <ion-card-subtitle>Location</ion-card-subtitle>
+              <ion-card-title>Mountain View, CA</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              <ion-icon name="glasses" slot="start"></ion-icon>
+              Cool cat tapping on the toe with a new hat, Ooh just cruising
+              (Cool Cat lyrics by Queen)
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      ))}
+    </ion-row>
+  );
 }
